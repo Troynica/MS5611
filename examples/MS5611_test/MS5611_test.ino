e9f7547f1826fb90bf23e5e6e80f88833e09f1bb
Roy <roy@Mac-Pro.local>
2019-01-16 15:55:47 +0100

    Cleaned up example sketch

--- examples/MS5611_test/MS5611_test.ino
+++ examples/MS5611_test/MS5611_test.ino
@@ -1,7 +1,7 @@
 //
 //    FILE: MS5611_test.ino
 //  AUTHOR: Rob Tillaart, modified by Roy van der Kraan
-// VERSION: 0.1.02a (modified for SPI version)
+// VERSION: 0.1.02mod
 // PURPOSE: demo application
 //    DATE: 2019-01-14
 //     URL: https://github.com/Troynica/MS5611
@@ -12,8 +12,9 @@
 #include "MS5611.h"
 #include <SPI.h>
 
-#define CS 10   // CS (CSB) pin
-MS5611 MS5611(CS);
+#define ADDR 10       // Address when using I2C,
+                      // CS (CSB) pin when using SPI (Arduino numbering)
+MS5611 MS5611(ADDR);
 
 
 void setup() {
@@ -26,8 +27,7 @@ void setup() {
   SPI.begin();
   MS5611.init();
 
-  unsigned int C_test[8]    = {224, 49529, 50674, 31079, 27913, 34690, 28381, 64164};  // for testing purposes, from an actual MS5611
-  unsigned int C_actual[8];
+  unsigned int C_read[8];
 
   uint8_t i;
   unsigned int n_rem = 0;
@@ -35,20 +35,20 @@ void setup() {
   unsigned char n_bit;
 
 
-  // Calculate and check PROM's CRC4 as per documentation
+  // Calculate and check PROM's CRC4
   // 
   int result = MS5611.read();
   for (i = 0; i < 8; i++)
   {
-    C_actual[i] = MS5611.getPromValue(i);
-    //C_actual[i] = C_test[i];
+    C_read[i] = MS5611.getPromValue(i);
+    //C_read[i] = C_test[i];
   }
-  crc_read = C_actual[7];
-  C_actual[7]=(0xFF00 & (C_actual[7]));
+  crc_read = C_read[7];
+  C_read[7]=(0xFF00 & (C_read[7]));
   for (i = 0; i < 16; i++)
   {
-    if (i%2==1) n_rem ^= (unsigned short) ( (C_actual[i>>1]) & 0x00FF );
-    else n_rem ^= (unsigned short) (C_actual[i>>1]>>8);
+    if (i%2==1) n_rem ^= (unsigned short) ( (C_read[i>>1]) & 0x00FF );
+    else n_rem ^= (unsigned short) (C_read[i>>1]>>8);
     for (n_bit = 8; n_bit > 0; n_bit--)
     {
       if (n_rem & (0x8000))
@@ -66,15 +66,16 @@ void setup() {
   Serial.print ("CRC (calculated) : ");
   Serial.println (n_rem,HEX);
   Serial.print ("CRC (read)       : ");
-  C_actual[7]=crc_read;
-  Serial.println (C_actual[7] & 15,HEX);
+  C_read[7]=crc_read;
+  Serial.println (C_read[7] & 15,HEX);
   Serial.println();
 }
 
 
 void loop()
 {
-    int result = MS5611.read(12);                          // 8 - 12 bit conversion; default 8
+    uint32_t test;
+    int result = MS5611.read(12);                          // 8 - 12 bit oversampling; default 8
     if (result != 0)
     {
         Serial.print("Error in read: ");
@@ -82,14 +83,12 @@ void loop()
     }
     else
     {
-        //uint16_t C5 = MS5611.getPromValue(5);            // request ROM parameter (0 - 7)
-        
-        Serial.print("Temp:\t");
+        //Serial.print("\tC6:\t");
+        //Serial.print(MS5611.getPromValue(6));
+        Serial.print("\tTemp:\t");
         Serial.print(MS5611.getTemperature() * 0.01, 2);  // print as float
         Serial.print("\t\tP:\t");
         Serial.print(MS5611.getPressure() * 0.01, 2);     // print as float
-        //Serial.print("\tC5:\t");
-        //Serial.print(C5);
         Serial.println();
     }
     delay(1000);
